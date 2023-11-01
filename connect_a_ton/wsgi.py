6b19ba691af2cd121520f990e2e66ef52b0276c1
Rohit T P <tprohit9@gmail.com>
2023-11-01 17:52:23 +0530

    Initial commit

---
+++ connect_a_ton/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for connect_a_ton project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect_a_ton.settings')
+
+application = get_wsgi_application()
