431322ab926fee1d7b072919e1824990518ba756
Rohit T P <tprohit9@gmail.com>
2023-11-01 19:32:33 +0530

    ui done

--- home/views.py
+++ home/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 
 @login_required
 def index(request):
-    return render(request, 'home/index.html')
+    context = {
+        "question": 'What is the meaning of life, the universe, and everything?',
+        "options": [
+            (0, "GJI"),
+            (1, "Make a Ton"),
+            (2, "42"),
+            (3, "I don't know")
+        ]
+    }
+    return render(request, 'home/index.html', context=context)
