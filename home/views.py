99698fc75f1645935936101334644d53a901b117
Rohit T P <tprohit9@gmail.com>
2023-11-01 20:38:29 +0530

    question shown

--- home/views.py
+++ home/views.py
@@ -11,30 +11,30 @@ def index(request):
     # get the user's question config
     config, _ = QuestionConfig.objects.get_or_create(user=request.user)
 
-    is_self = random.choice([True, False])
+    is_self = random.choice([True])
 
     question, answer = None, None
 
     if is_self:
         question = Question.objects.exclude(id__in=config.self_questions.all()).first()
-        if question:
-            answer = Answer.objects.create(question=question, user=request.user, actual_answer=random.randint(23, 456), answer=-1)
-    else:
-        answer = Answer.objects.exclude(id__in=config.other_answers.all()).first()
 
-    if is_self and answer is None:
+    if question:
+        answer, _ = Answer.objects.get_or_create(question=question, user=request.user, actual_answer=-8,
+                                                 answer_value=-1)
+    else:
         is_self = False
-        answer = Answer.objects.exclude(id__in=config.other_answers.all()).first()
+        answer = Answer.objects.exclude(id__in=config.other_answers.all()).exclude(user=request.user).first()
 
     if answer is None:
         return render(request, 'home/index.html')
 
     question = answer.question
-    name = request.user.username if is_self else answer.user.username
+    name = "your" if is_self else answer.user.username
 
     context = {
         "question": question.question_text.replace("%USER%", name),
         "options": question.options,
         "answer": answer.id,
+        "is_self": is_self,
     }
     return render(request, 'home/index.html', context=context)
