a1e1919b4ed7709beeb85f40dd484293a797befb
rohit <rohittp0@github.com>
2023-11-05 01:32:22 +0530

    deliver

--- swags/admin.py
+++ swags/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from home.models import UserConfig
 from .models import Swag, SwagAward
 
 
@@ -12,5 +13,17 @@ class SwagAdmin(admin.ModelAdmin):
 @admin.register(SwagAward)
 class SwagAwardAdmin(admin.ModelAdmin):
     list_filter = ('delivered',)
-    list_display = ('swag', 'user', 'delivered')
+    list_display = ('swag', 'team', 'user', 'delivered')
     search_fields = ('swag__description', 'user__username')
+
+    actions = ['mark_as_delivered']
+
+    @staticmethod
+    def team(obj):
+        return UserConfig.objects.get(user=obj.user).team
+
+    @staticmethod
+    def mark_as_delivered(self, request, queryset):
+        queryset.update(delivered=True)
+
+    mark_as_delivered.short_description = 'Mark selected swag as delivered'
